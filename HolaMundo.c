8f0f336f665b4575d17af47c543d2bee86910434
MartinchoGz <smartinjimenez95@gmail.com>
2020-08-28 22:13:19 -0300

    Ultima modificacion

--- HolaMundo.c
+++ HolaMundo.c
@@ -7,7 +7,7 @@ int main(){
   scanf("%d", numEmp);
   printf("Bienvenido Empleado N°%d.\n", numEmp);
   printf("Ha llegado a la rama Developers.");
-  scanf("%d", numEmp);
+  #scanf("%d", numEmp);
   printf("Estoy en la rama de edición: Martín.");
   
   return 0;
