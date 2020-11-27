7ce279fa927dd9fb379200c767075e52158c4b73
MartinchoGz <smartinjimenez95@gmail.com>
2020-11-26 21:54:54 -0300

    Agrego funcion de multiplicacion

--- Codificando.c
+++ Codificando.c
@@ -2,6 +2,8 @@
 
 // Esto es un comentario de prueba.
 
+int multiplicarPorTres(int i);
+
 int main(){
   int i = 0;
   
@@ -11,4 +13,8 @@ int main(){
   }
   
   return 0;
+}
+
+int multiplicarPorTres(int i){
+  return (i * 3);
 }
